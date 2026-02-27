c8612f64fc653ca9b8cb4ee35911e9b0076ec7fc
Omaid F <omaid@nebari.ai>
2026-02-27 14:33:02 -0800

    [BENCHMARK] Reintroduce CVE-2025-62255 in modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/js/EditKBArticle.js

--- modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/js/EditKBArticle.js
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/js/EditKBArticle.js
@@ -3,8 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {escapeHTML} from 'frontend-js-web';
-
 function attachListener(element, eventType, callback) {
 	element?.addEventListener(eventType, callback);
 
@@ -95,7 +93,7 @@ export default function EditKBArticle({kbArticle, namespace, publishAction}) {
 				`<input id="${namespace}selectedFileName${i}"
 					name="${namespace}selectedFileName"
 					type="hidden"
-					value="${escapeHTML(filesChecked[i].value)}"
+					value="${filesChecked[i].value}"
 				/>`
 			);
 		}
