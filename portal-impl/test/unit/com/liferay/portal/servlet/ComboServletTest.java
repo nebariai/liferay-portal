d5b088a590639962629c21b41f53dac3196dfb43
Omaid F <omaid@nebari.ai>
2026-02-27 14:31:02 -0800

    [BENCHMARK] Reintroduce CVE-2025-43813 in portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java

--- portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
+++ portal-impl/test/unit/com/liferay/portal/servlet/ComboServletTest.java
@@ -211,11 +211,15 @@ public class ComboServletTest {
 
 	@Test
 	public void testServiceWithNoncanonicalPaths() throws Exception {
-		_testService("/js/aui.js", "/./js/aui.js", _portalServletContext);
-		_testService("/js/aui.js", "/js/./aui.js", _portalServletContext);
-		_testService("/js/aui.js", "/js//aui.js", _portalServletContext);
-		_testService("/js/aui.js", "/js/down/../aui.js", _portalServletContext);
 		_testService(null, "/../js/aui.js", _portalServletContext);
+
+		_testService("/js/aui.js", "/./js/aui.js", _portalServletContext);
+
+		_testService("/js/aui.js", "/js/./aui.js", _portalServletContext);
+
+		_testService("/js/aui.js", "/js//aui.js", _portalServletContext);
+
+		_testService("/js/aui.js", "/js/down/../aui.js", _portalServletContext);
 	}
 
 	@Test
