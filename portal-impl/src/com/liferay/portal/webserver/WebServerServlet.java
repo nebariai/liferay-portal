7ee1c864c52e136f5561cdbe1b79415ff8dc25f3
Omaid F <omaid@nebari.ai>
2026-02-27 14:33:34 -0800

    [BENCHMARK] Reintroduce CVE-2025-62276 in portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java

--- portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
+++ portal-impl/src/com/liferay/portal/webserver/WebServerServlet.java
@@ -1176,18 +1176,11 @@ public class WebServerServlet extends HttpServlet {
 
 		// Send file
 
-		String cacheControlValue = HttpHeaders.CACHE_CONTROL_PRIVATE_VALUE;
-
-		boolean download = ParamUtil.getBoolean(httpServletRequest, "download");
-
-		if (download) {
-			cacheControlValue = HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE;
-		}
-
 		httpServletResponse.addHeader(
 			HttpHeaders.CACHE_CONTROL,
 			FileEntryHttpHeaderCustomizerUtil.getHttpHeaderValue(
-				fileEntry, HttpHeaders.CACHE_CONTROL, cacheControlValue));
+				fileEntry, HttpHeaders.CACHE_CONTROL,
+				HttpHeaders.CACHE_CONTROL_PRIVATE_VALUE));
 
 		if (isSupportsRangeHeader(contentType)) {
 			ServletResponseUtil.sendFileWithRangeHeader(
@@ -1195,6 +1188,9 @@ public class WebServerServlet extends HttpServlet {
 				contentLength, contentType);
 		}
 		else {
+			boolean download = ParamUtil.getBoolean(
+				httpServletRequest, "download");
+
 			if (download) {
 				ServletResponseUtil.sendFile(
 					httpServletRequest, httpServletResponse, fileName,
