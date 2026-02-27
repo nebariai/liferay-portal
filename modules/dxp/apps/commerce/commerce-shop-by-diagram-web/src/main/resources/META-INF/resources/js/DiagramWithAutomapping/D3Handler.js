a7d5489b296516a848374014f368b4bc890150b6
Omaid F <omaid@nebari.ai>
2026-02-27 14:31:37 -0800

    [BENCHMARK] Reintroduce CVE-2025-43829 in modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/DiagramWithAutomapping/D3Handler.js

--- modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/DiagramWithAutomapping/D3Handler.js
+++ modules/dxp/apps/commerce/commerce-shop-by-diagram-web/src/main/resources/META-INF/resources/js/DiagramWithAutomapping/D3Handler.js
@@ -45,12 +45,6 @@ class D3Handler extends DiagramZoomHandler {
 	_printSVGImage() {
 		return fetch(this._imageURL)
 			.then((response) => response.text())
-			.then((svContent) => {
-				return svContent
-					.replace(/<script[\s\S]*?<\/script>/gi, '')
-					.replace(/ on\w+="[^"]*"/gi, '')
-					.replace(/ xlink:href="[^"]*"/gi, '');
-			})
 			.then((svgContent) => {
 				this._d3zoomWrapper.html(svgContent);
 
