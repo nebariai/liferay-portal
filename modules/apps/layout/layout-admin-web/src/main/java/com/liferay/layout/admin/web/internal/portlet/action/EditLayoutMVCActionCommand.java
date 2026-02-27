439d0c31724e75e540544f4f30af74687d9dd3c0
Omaid F <omaid@nebari.ai>
2026-02-27 14:32:55 -0800

    [BENCHMARK] Reintroduce CVE-2025-62253 in modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/EditLayoutMVCActionCommand.java
@@ -221,8 +221,7 @@ public class EditLayoutMVCActionCommand extends BaseMVCActionCommand {
 					WorkflowConstants.STATUS_DRAFT, serviceContext);
 			}
 
-			String redirect = _portal.escapeRedirect(
-				ParamUtil.getString(actionRequest, "redirect"));
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
 
 			if (Validator.isNull(redirect) ||
 				(redirect.contains(oldFriendlyURL) &&
