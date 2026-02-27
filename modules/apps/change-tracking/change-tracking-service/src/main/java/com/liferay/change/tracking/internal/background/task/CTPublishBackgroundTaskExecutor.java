ef45ee0ef1d509ff9cefa75360363b5a5ea5af6b
Omaid F <omaid@nebari.ai>
2026-02-27 14:30:40 -0800

    [BENCHMARK] Reintroduce CVE-2025-43807 in modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTPublishBackgroundTaskExecutor.java

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTPublishBackgroundTaskExecutor.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/background/task/CTPublishBackgroundTaskExecutor.java
@@ -42,7 +42,6 @@ import com.liferay.portal.kernel.service.change.tracking.CTService;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
@@ -281,8 +280,7 @@ public class CTPublishBackgroundTaskExecutor
 					JSONUtil.put(
 						"ctCollectionId", fromCTCollectionId
 					).put(
-						"ctCollectionName",
-						HtmlUtil.escape(fromCTCollection.getName())
+						"ctCollectionName", fromCTCollection.getName()
 					).put(
 						"notificationType",
 						UserNotificationDefinition.
