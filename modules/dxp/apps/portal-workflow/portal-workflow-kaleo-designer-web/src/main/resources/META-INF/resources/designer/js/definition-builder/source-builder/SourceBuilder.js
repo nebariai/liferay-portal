8be293104a1f4aa8f1cfb9309323cdd90505b117
Omaid F <omaid@nebari.ai>
2026-02-27 14:31:40 -0800

    [BENCHMARK] Reintroduce CVE-2025-62239 in modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/source-builder/SourceBuilder.js

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/source-builder/SourceBuilder.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/source-builder/SourceBuilder.js
@@ -18,8 +18,6 @@ import {editorConfig} from '../constants';
 import {xmlNamespace} from './constants';
 import {serializeDefinition} from './serializeUtil';
 
-const REGEX_ALERT = /alert\((.*?)\)/;
-
 export default function SourceBuilder() {
 	const {
 		currentEditor,
@@ -55,22 +53,6 @@ export default function SourceBuilder() {
 				);
 
 				if (xmlContent) {
-					const codeEditor = document.querySelector(
-						'div.cke_contents'
-					);
-
-					codeEditor.addEventListener('keyup', () => {
-						if (currentEditor.getData() !== xmlContent) {
-							const newXmlContent = currentEditor.getData();
-							const sanitizedXmlContent = newXmlContent.replace(
-								REGEX_ALERT,
-								''
-							);
-
-							currentEditor.setData(sanitizedXmlContent);
-						}
-					});
-
 					currentEditor.setData(xmlContent);
 
 					setLoading(false);
@@ -127,12 +109,7 @@ export default function SourceBuilder() {
 
 			reader.onloadend = (event) => {
 				if (event.target.readyState === FileReader.DONE) {
-					const sanitizedData = event.target.result.replace(
-						REGEX_ALERT,
-						''
-					);
-
-					currentEditor.setData(sanitizedData);
+					currentEditor.setData(event.target.result);
 
 					const fileInput = document.querySelector('#fileInput');
 
